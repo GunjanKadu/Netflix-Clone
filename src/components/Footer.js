ce87fbf70e264073de6a9898f4e3c38d204a50ce
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-29 16:47:52 +0200

    Footer Componetn and navigation styles updated

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const footer = () => (
+  <footer className='footer'>
+    <div className='footer__copyright'>
+      &copy; 2020 Made by{' '}
+      <a
+        className='footer__copyright--link'
+        href='https://gunjankadu.github.io/Portfolio/'
+      >
+        {' '}
+        Gunjan Kadu
+      </a>
+    </div>
+  </footer>
+);
+
+export default footer;
